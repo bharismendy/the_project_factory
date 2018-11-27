65a9de4282e08ab4fc5eddbc8d35ebdee1e6f034
etudiant <brice.harismendy@etud.univ-angers.fr>
2018-11-27 08:20:55 +0100

    ajout du script d'installation de l'environement virtuel

---
+++ the_project_factory/install_venv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "suppression du dossier venv..."
+rm -rf venv
+echo "création de l'environement virtuel..."
+virtualenv venv -p python3
+echo "installation des package python..."
+source venv/bin/activate
+pip install -r requirement.txt 
+deactivate
+echo "fin de l'installation de l'environement virtuel ..."
+
